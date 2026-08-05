6119514414eb191228726b2cf52a7707d872a6cb
saatvika.u <saatvika.u@gmail.com>
2026-08-05 15:22:29 +0530

    Init repo

---
+++ script.js
@@ -0,0 +1,18 @@
+function updateClock() {
+    const now = new Date();
+
+    let hours = now.getHours();
+    const meridian = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, "0");
+
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+
+    document.getElementById("clock").innerHTML =
+        `${hours}:${minutes}:${seconds} <span class="ampm">${meridian}</span>`;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
